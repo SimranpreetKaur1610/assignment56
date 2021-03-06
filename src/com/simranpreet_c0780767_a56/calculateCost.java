363b9799a24c530f006fd3f9a00c771a01ddc13f
Simranpreetkaur <harsim610@gmail.com>
2021-03-06 01:11:29 -0500

    getter and setter

--- src/com/simranpreet_c0780767_a56/calculateCost.java
+++ src/com/simranpreet_c0780767_a56/calculateCost.java
@@ -1,4 +1,59 @@
 package com.simranpreet_c0780767_a56;
 
-public class calculateCost {
+public class calculateCost
+{
+    double packageACost;
+    double packageBCost;
+    double smallStorageCost;
+    double largeStorageCost;
+    double boxesSmallcost;
+    double boxesLargecost;
+
+    public double getPackageACost() {
+        return packageACost;
+    }
+
+    public void setPackageACost(double packageACost) {
+        this.packageACost = 100.00;
+    }
+
+    public double getPackageBCost() {
+        return packageBCost;
+    }
+
+    public void setPackageBCost(double packageBCost) {
+        this.packageBCost = 150.00;
+    }
+
+    public double getSmallStorageCost() {
+        return smallStorageCost;
+    }
+
+    public void setSmallStorageCost(double smallStorageCost) {
+        this.smallStorageCost = 8.00;
+    }
+
+    public double getLargeStorageCost() {
+        return largeStorageCost;
+    }
+
+    public void setLargeStorageCost(double largeStorageCost) {
+        this.largeStorageCost = 20.11;
+    }
+
+    public double getBoxesSmallcost() {
+        return boxesSmallcost;
+    }
+
+    public void setBoxesSmallcost(double boxesSmallcost) {
+        this.boxesSmallcost = 2.50;
+    }
+
+    public double getBoxesLargecost() {
+        return boxesLargecost;
+    }
+
+    public void setBoxesLargecost(double boxesLargecost) {
+        this.boxesLargecost = 4.50;
+    }
 }
