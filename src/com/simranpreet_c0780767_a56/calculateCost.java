246eb4bf41827255b681a2e928de6abd391bd0b5
Simranpreetkaur <harsim610@gmail.com>
2021-03-10 02:39:19 -0500

    Final Changes

--- src/com/simranpreet_c0780767_a56/calculateCost.java
+++ src/com/simranpreet_c0780767_a56/calculateCost.java
@@ -69,49 +69,50 @@ public class calculateCost
     }
 
     public static void main(String[] args) {
-        calculateCost cost = new calculateCost();
+        calculateCost totalCost = new calculateCost();
 
         String input;
         int j = 0;
-        JOptionPane.showMessageDialog(null,"Please Enter Simran as username and Kaur as password");
+        JOptionPane.showMessageDialog(null,"Use 'simran' as username and password to login");
         while (true){
             String userName = JOptionPane.showInputDialog(
                     null,"Please enter username ");
             String userPassword = JOptionPane.showInputDialog(
                     null,"Please enter the password ");
-            if(userName.equals("Simran") && userPassword.equals("Kaur")){
+            if(userName.equals("simran") && userPassword.equals("simran")){
                 JOptionPane.showMessageDialog(null,
                         "You have successfully logged in");
-                double servicePackage = getServices(cost.getPackageACost(),cost.getPackageBCost());
+                double servicePackage = getServices(totalCost.getPackageACost(),totalCost.getPackageBCost());
                 input = JOptionPane.showInputDialog(null,"How many hours you want this service: ");
                 double hoursService = Double.parseDouble(input);
                 double totalService = servicePackage * hoursService;
                 System.out.println("Service cost : $"+totalService);
 
-                double storageOption = getOptions(cost.getSmallStorageCost(),cost.getLargeStorageCost());
+                double storageOption = getOptions(totalCost.getSmallStorageCost(),totalCost.getLargeStorageCost());
                 input = JOptionPane.showInputDialog(null,"How many days you want this option: ");
                 double dayOptions = Double.parseDouble(input);
                 double totalOptions = storageOption * dayOptions;
                 System.out.println("Options of storage cost : $"+totalOptions);
 
-                double boxOption = getBoxOption(cost.getBoxesSmallcost(),cost.getBoxesLargecost());
+                double boxOption = getBoxOption(totalCost.getBoxesSmallcost(),totalCost.getBoxesLargecost());
                 input = JOptionPane.showInputDialog(null,"How many boxes you want this item: ");
                 double boxItems = Double.parseDouble(input);
                 double totalBoxes = boxOption * boxItems;
                 System.out.println("Item cost : $"+totalBoxes);
 
                 JOptionPane.showMessageDialog(null,
-                        userName+"'s Receipt"+
-                                "\nService cost : $"+ totalService+
-                                "\n\nOptions of storage cost : $"+ totalOptions +
-                                "\n\nItem cost : $"+ totalBoxes+
-                                "\n\nThe total final cost : $\n"+ (totalService + totalOptions + totalBoxes)
+                        userName+"'s Receipt\n"+
+                                "--------------------------------"+
+                                "\n\nService cost : $"+ totalService+
+                                "\nOptions of storage cost : $"+ totalOptions +
+                                "\nItem cost : $"+ totalBoxes+
+                                "\n\nThe total final cost : $"+ (totalService + totalOptions + totalBoxes)
                         );
-                System.out.println("The total final cost : $\n"+(totalService + totalOptions + totalBoxes));
+                System.out.println("\nThe total final cost : $"+(totalService + totalOptions + totalBoxes));
                 break;
             }else{
                 JOptionPane.showMessageDialog(null,
-                        "Invald Login!!! Please Eter the username and password again.");
+                        "Invald Login!!! Please Enter the username and password again.");
             }
             j+=1;
             if(j == 3){
